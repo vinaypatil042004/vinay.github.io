acec944925978fe33657de037407d2d9f3ffea72
vinaypatil042004 <vinaypatl042@gmail.com>
2024-06-15 12:42:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function showDetails(id) {
+function toggleDetails(id) {
     var details = document.getElementById(id);
     if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block";
